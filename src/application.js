c4de4604c95e7907770e3320edc0dd26903f579f
danwheeler <da-wheeler@hotmail.co.uk>
2015-11-12 16:20:19 +0000

    jquery added

---
+++ src/application.js
@@ -0,0 +1,18 @@
+	window.onload = function() {
+	 
+	    alert( "welcome" );
+	 
+	};
+
+	 $( "#temperature-up" ).button().click( function() {
+
+      alert( "Thanks for visiting!" );
+
+  });
+ 
+
+// $( document ).ready(function() {
+ 
+ 
+// });
+
