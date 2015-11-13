134fe905a8e8ae2ef5b04ce709a6646fe7caf88e
blancopado <pablo.dominguezblanco@gmail.com>
2015-11-13 15:04:46 +0100

    temp up button

--- src/application.js
+++ src/application.js
@@ -2,8 +2,13 @@ window.onload = function() {
     alert( "welcome" );
 };
 
+thermostat = new Thermostat();
+$('#temperature').text(thermostat.temp);
+
 $( document ).ready(function() {
 	 $( "#temperature-up" ).click( function() {
-      alert( "Thanks for visiting!" );
+      thermostat.up();
+      console.log(thermostat.temp)
+      $('#temperature').text(thermostat.temp);
   });
 });
