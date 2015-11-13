afdfb06577404b22cb5f5425e99d505b49a1204b
Tom Barnes <ruralmyth@gmail.com>
2015-11-13 14:29:09 +0000

    add temperature helper function in jQuery

--- src/application.js
+++ src/application.js
@@ -1,28 +1,34 @@
-thermostat = new Thermostat();
-$('#temperature').text(thermostat.temp);
-
 $( document ).ready(function() {
-	 $( "#temperature-up" ).click( function() {
-      thermostat.up();
-      $('#temperature').text(thermostat.temp);
+
+  thermostat = new Thermostat();
+
+  function temperature() {
+    $('#temperature').text(thermostat.temp);
+  }
+
+  temperature();
+
+ $( "#temperature-up" ).click( function() {
+     thermostat.up();
+     temperature();
   });
    $( "#temperature-down" ).click( function() {
       thermostat.down();
-      $('#temperature').text(thermostat.temp);
+      temperature()
   });
    $( "#temperature-reset" ).click( function() {
       thermostat.resetButton();
-      $('#temperature').text(thermostat.temp);
+      temperature()
   });
   $( "#powersaving-on" ).click( function() {
     thermostat.powerSavingModeOn();
-    $('#temperature').text(thermostat.temp);
+    temperature()
     $(this).css('background-color', 'green')
     $('#powersaving-off').css('background-color', 'white')
   });
   $( "#powersaving-off" ).click( function() {
     thermostat.powerSavingModeOff();
-    $('#temperature').text(thermostat.temp);
+    temperature()
     $(this).css('background-color', 'red')
     $('#powersaving-on').css('background-color', 'white')
   });
